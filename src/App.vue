2e23cc857fe40f9eb592f61cdf0777ba6cdd1cb8
Gadya <zakuson2001@inbox.ru>
2020-10-11 23:54:29 +0300

    start todo-list via vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <todo />
+  </div>
+</template>
+
+<script>
+import todo from './components/todo.vue';
+
+export default {
+  components: {
+    todo,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/todo.vue
@@ -0,0 +1,74 @@
+<template>
+<div class = 'page'>
+  <h3> TODO List: </h3>
+  <b-button class="add-task">Добавить задачу</b-button>
+  <b-table small fixed striped hover :fields="fields" :items="items">
+    <template v-slot:cell(Внести изменения)="row">
+      <b-button size="sm" @click="row.toggleDetails" class="mr-2">Изменить</b-button>
+    </template>
+    <template v-slot:cell(Выбор)="row">
+      <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails"></b-form-checkbox>
+    </template>
+  </b-table>
+  <div class="done-btn-div">
+    <b-button class="done-btn">Завершить выбранные</b-button>
+    <b-button class="delete-btn">Удалить выбранные</b-button>
+  </div>
+  <!-- <div v-for='(todo,key) in todo_list' :key='key'>
+      <p>{{ todo.id }} | {{ todo.describe }} | {{ todo.status }} </p>
+  </div> -->
+</div>
+</template>
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default {
+  name: 'todo.vue',
+  data() {
+    const items = [
+      {
+        'Номер п/п': 1,
+        'Задача': 'Освоить Vue',
+        'Статус': false,
+      },
+      // {
+      //   id: 2,
+      //   describe: 'Пройти аттестацию модуля С',
+      //   status: false,
+      // },
+      // {
+      //   id: 3,
+      //   describe: 'Выпить пивка в субботу',
+      //   status: false,
+      // },
+    ];
+    return {
+      fields: ['Номер п/п', 'Задача', 'Статус', 'Внести изменения', 'Выбор'],
+      items,
+    };
+  },
+};
+</script>
+<style scoped>
+h3 {
+    text-align: center;
+    color: aqua;
+}
+.page {
+  width: 60%;
+  margin: 0 auto;
+}
+.add-task{
+  padding: 3px;
+  margin-bottom: 5px;
+  font-size: 0.8rem;
+}
+.done-btn-div{
+  display: flex;
+  justify-content: flex-end;
+}
+.done-btn, .delete-btn {
+  margin: 0 0 0 10px;
+}
+
+</style>
